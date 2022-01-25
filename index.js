786c3806f3ada4c7786cd0e9f5ee12a17a720202
carlocardona <carlo.dev2@gmail.com>
2022-01-26 10:56:06 +1100

    got API hiding and get response working

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+require('dotenv').config();
+const hellosign = require ('hellosign-sdk')({key: process.env.CC_TEST_API_KEY})
+
+hellosign.account.get().then((res) => {
+    //handle response 
+    console.log(res)
+}).catch((err) => {
+    //handle error
+    console.log(err);
+})
