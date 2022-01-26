ed3aa1b72fca60b6e23eb17841978fe27716ac49
carlocardona <carlo.dev2@gmail.com>
2022-01-26 11:43:34 +1100

    using inquirer for terminal interactivity

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
+const inquirer = require('inquirer');
 const hellosign = require ('hellosign-sdk')({key: process.env.CC_TEST_API_KEY})
 
-hellosign.account.get().then((res) => {
-    //handle response 
-    console.log(res)
-}).catch((err) => {
-    //handle error
-    console.log(err);
+inquirer.prompt([
+    {
+        type:"input",
+        message:"Choose 1",
+        name:"choice"
+    }
+]).then(function(res){
+
+    if (res.choice === '1'){
+
+        console.log("Response 1 Hit");
+        hellosign.account.get().then((res) => {
+            console.log(res);
+
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
 })
