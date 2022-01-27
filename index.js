351105a96852f1d852c8a560b54beb28a97217de
carlocardona <carlo.dev2@gmail.com>
2022-01-27 14:57:35 +1100

    working of some GET methods

--- index.js
+++ index.js
@@ -31,5 +31,70 @@ inquirer.prompt([
 });
 
 function otherGET() {
-    console.log("Other Get Called")
+
+    const opts = {
+      test_mode: 1,
+    };
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "otherGET",
+          message: "Other Get Requests",
+          choices: ["Sign  URL", "Signature Request", "Template", "API App"],
+        },
+      ])
+      .then(function (res) {
+        switch (res.otherGET) {
+          case "Sign  URL":
+            hellosign.signatureRequest
+              .createEmbedded(opts)
+              .then((res) => {
+                const signature = res.signature_request.signatures[0];
+                const signatureId = signature.signature_id;
+
+                return hellosign.embedded.getSignUrl(signatureId);
+              })
+              .then((res) => {
+                console.log("The Sign URL: " + res.embedded.sign_url);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+            break;
+          case "Signature Request":
+            console.log("Signature Request");
+
+            inquirer
+              .prompt([
+                {
+                  type: "input",
+                  name: "signatureRequest",
+                  message: "Enter Signature Request ID: ",
+                },
+              ])
+              .then(function (res) {
+                let signatureRequestId = res.signatureRequest;
+                hellosign.signatureRequest
+                  .get(signatureRequestId)
+                  .then((res) => {
+                    console.log(res);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              });
+
+            break;
+          case "Template":
+            console.log("Template");
+            break;
+          case "API App":
+            console.log("API App");
+            break;
+          default:
+            break;
+        }
+      });
 }
